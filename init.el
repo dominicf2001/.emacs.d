7b380cc85f31af829269d9ab6977742bf52fa5e5
dominic <dominicf2001@gmail.com>
2024-01-17 14:20:20 -0500

    updates

--- init.el
+++ init.el
@@ -15,15 +15,11 @@
       (package-initialize))  ;; be sure load-path includes package directories
     (require 'use-package)))
 
-;; Set font
-(set-face-attribute 'default nil :font "Iosvmata" :height 170)
-(setq default-frame-alist '((font . "Iosvmata")))
-
 ;; Set org babel's load file
 (org-babel-load-file "/home/dominic/.emacs.d/config.org")
 
 ;; Load nerd icons (MAY REMOVE)
-;; (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)
+(add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)
 
 ;; ---------------
 
@@ -35,10 +31,12 @@
  '(custom-safe-themes
    '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" default))
  '(package-selected-packages
-   '(lsp-pyright go-mode flycheck typescript-mode nerd-icons-corfu corfu yasnippet-snippets yasnippet lsp-ido tree-sitter-langs tree-sitter zenburn-theme zenburn geiser-mit cmake-mode use-package)))
+   '(vertico-directory marginalia vertico xresources-theme lsp-pyright go-mode flycheck typescript-mode nerd-icons-corfu corfu yasnippet-snippets yasnippet lsp-ido tree-sitter-langs tree-sitter zenburn-theme zenburn geiser-mit cmake-mode use-package))
+ '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(highlight ((t (:background "gray18"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "orange")))))
